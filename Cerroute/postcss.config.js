c8105ec1d44d73e5a293d6634c67d177c6282614
melikbilyay <melikbilyay@hotmail.com>
2024-04-08 06:50:18 +0300

    v4

--- Cerroute/postcss.config.js
+++ Cerroute/postcss.config.js
@@ -1,13 +1,10 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
-module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-};
+}
--- Cerroute/tailwind.config.js
+++ Cerroute/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: [],
-  darkMode: true, // or 'media' or 'class'
   theme: {
     extend: {},
   },
